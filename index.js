34b90dbb2d62b46274dcc450acc2df658180ee6a
siroga0303 <71637463+siroga0303@users.noreply.github.com>
2023-07-06 20:05:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const rp = require('request-promise');
 const cher = require('cheerio');
 const htmlparser2 = require('htmlparser2');
 const cors = require('cors')
-const url = 'https://boardgamegeek.com/browse/boardgame';
+let numberPage;
+const url = `https://boardgamegeek.com/browse/boardgame/page/${numberPage}`;
 const app = express()
 const port = 3000
-
+let Jsondata;
 const corsOptions = {
     origin: '*',
     methods: [],
@@ -14,6 +15,9 @@ const corsOptions = {
     exposedHeaders: [],
     credentials: true
 };
+
+let boards = [];
+
 
 async function getJson() {
     const html = await rp(url);
@@ -23,11 +27,12 @@ async function getJson() {
     const wikiID = [];
     const dom = htmlparser2.parseDocument(html);
     const $ = cher.load(dom);
-    let len = $('div[id *= "results_objectname"] a[href *= "/boardgame"]', html).length;
+    let len = $('td[class *= "collection_thumbnail"] a[href *= "/boardgame"]', html).length;
     
     for (let i = 0; i < len; i++) {
-        wikiUrls.push($('div[id *= "results_objectname"] a[href *= "/boardgame"]', html)[i].attribs.href);
-    }
+        wikiUrls.push($('td[class *= "collection_thumbnail"] a[href *= "/boardgame"]', html)[i].attribs.href);
+        boards.push( { imgUrl: $('td[class *= "collection_thumbnail"] a[href *= "/boardgame"] img', html)[i].attribs.src , imgAlt: $('td[class *= "collection_thumbnail"] a[href *= "/boardgame"] img', html)[i].attribs.alt})
+    } 
     
     const re = new RegExp("/[A-Za-z]+/[0-9]+");
     
@@ -37,17 +42,49 @@ async function getJson() {
         
     }
     
-    var myJsonString = JSON.stringify(wikiID);
     
-    return myJsonString
+    
+    return wikiID
     
   
   
 }
+
+async function getGamesData() {
+    Jsondata = await getJson();
+    let xml;
+    for(let a = 0; a < Jsondata.length; a++){
+        console.log(a)
+        xml = await rp(`https://boardgamegeek.com/xmlapi/boardgame/${Jsondata[a]}?stats=1`);
+        const dom = htmlparser2.parseDocument(xml);
+        const $ = cher.load(dom);
+        boards[a].minplaytime = $("minplaytime", dom).text()
+        boards[a].maxplaytime = $("maxplaytime", dom).text()
+        let totalvotes = $('poll[name *= "suggested_numplayers"]', dom).attr().totalvotes
+        boards[a].totalvotes = totalvotes
+        boards[a].rating = $('ratings average', dom).text()
+        let len = $('poll[name *= "suggested_numplayers"] results result[value *= "Best"]', dom).length
+        for (let i = 0; i< len; i++) {
+            boards[a][`player_${i+1}`] = $('poll[name *= "suggested_numplayers"] result[value *= "Best"]', dom)[i].attribs.numvotes
+            
+        }
+    } 
+    
+    
+    
+    
+    
+    
+    
+
+    
+
+     return JSON.stringify(boards)
+}
 let a;
-app.get('/', cors(corsOptions), async (req, res, next) => {
-    console.log('ID:')
-    a = await getJson()
+app.get('/get', cors(corsOptions), async (req, res, next) => {
+    numberPage = req.query.id
+    a = await getGamesData()
     
     next()
   }, (req, res, next) => {
